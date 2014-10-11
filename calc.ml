daa59975d3beaf15a469f93c7444ed69dd9e5a1e
Martin Ong <mo2454@columbia.edu>
2014-10-11 14:30:04 -0400

    Added variables and sequencing
    From homework 1 problem 3

--- calc.ml
+++ calc.ml
@@ -1,17 +1,22 @@
 open Ast
 
-let rec eval = function 
-    Lit(x) -> x
-  | Binop(e1, op, e2) ->
-      let v1 = eval e1 and v2 = eval e2 in
-      match op with
-	Add -> v1 + v2
-      | Sub -> v1 - v2
-      | Mul -> v1 * v2
-      | Div -> v1 / v2
+let var = Array.make 10 0
+
+let rec eval = function
+	  Lit(x) -> x
+	| Seq(fir,sec) -> ignore(eval fir) ; eval sec
+	| Asn(v, e) -> ignore(var.(v) <- eval e) ; eval e
+	| Var(v) -> var.(v)
+	| Binop(e1, op, e2) ->
+		let v1 = eval e1 and v2 = eval e2 in
+		match op with
+			  Add -> v1 + v2
+			| Sub -> v1 - v2
+			| Mul -> v1 * v2
+			| Div -> v1 / v2
 
 let _ =
-  let lexbuf = Lexing.from_channel stdin in
-  let expr = Parser.expr Scanner.token lexbuf in
-  let result = eval expr in
-  print_endline (string_of_int result)
+	let lexbuf = Lexing.from_channel stdin in
+	let expr = Parser.expr Scanner.token lexbuf in
+	let result = eval expr in
+	print_endline (string_of_int result)
--- parser.mly
+++ parser.mly
@@ -2,18 +2,26 @@
 
 %token PLUS MINUS TIMES DIVIDE EOF
 %token <int> LITERAL
+%token <int> VARIABLE
+%token ASSIGNMENT SEQUENCE
 
+%left SEQUENCE
+%right ASSIGNMENT
 %left PLUS MINUS
 %left TIMES DIVIDE
 
 %start expr
-%type < Ast.expr> expr
+%type <Ast.expr> expr
 
 %%
 
 expr:
-  expr PLUS   expr { Binop($1, Add, $3) }
-| expr MINUS  expr { Binop($1, Sub, $3) }
-| expr TIMES  expr { Binop($1, Mul, $3) }
-| expr DIVIDE expr { Binop($1, Div, $3) }
-| LITERAL          { Lit($1) }
+	  expr PLUS expr { Binop($1, Add, $3) }
+	| expr MINUS expr { Binop($1, Sub, $3) }
+	| expr TIMES expr { Binop($1, Mul, $3) }
+	| expr DIVIDE expr { Binop($1, Div, $3) }
+	| LITERAL { Lit($1) }
+
+	| VARIABLE { Var($1) }
+	| VARIABLE ASSIGNMENT expr { Asn($1, $3) }
+	| expr SEQUENCE expr { Seq($1, $3) }
