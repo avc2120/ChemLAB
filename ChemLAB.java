a3a1787fd9f556e3c509d3f39bbd7047881d70b3
Alice Chang <avc2120@columbia.edu>
2014-12-05 23:02:35 -0500

    outputs equation

--- ChemLAB.java
+++ ChemLAB.java
@@ -10,12 +10,13 @@ import java.util.Scanner;
 public class ChemLAB 
 {
     public static Scanner scan;
-    public static int n;
+    public static boolean debug = false;
     public static void Balance(String s)
     {
         String[] r = s.split("(, )|(==)|(' ')");
+        String[] r1 = s.split("\\s*(,|\\s)\\s*");
         String[] r2 = s.split("(, )|(' ')");
-        String[] individual = s.split("(, )|(==)|(?=\\p{Upper})|(' ')");
+        String[] individual = s.split("(, )|(== )|(?=\\p{Upper})|(' ')");
         
         ArrayList<String> elements = new ArrayList<String>();
 
@@ -58,11 +59,7 @@ public class ChemLAB
             }
         }
         
-        for(int i=0; i<matrix.length; i++){
-            for(int j=counter; j<matrix[i].length-1; j++){
-                matrix[i][j] = matrix[i][j] * -1;
-            }
-        }
+
 
         double[][] A = new double[matrix.length][matrix[0].length - 1];
         double[][] B = new double[matrix.length][1];
@@ -72,38 +69,133 @@ public class ChemLAB
                 A[i][j] = matrix[i][j];
             }
         }
+
+        int n = A[0].length<A.length? A.length : A[0].length;
+        int difference = Math.abs(A.length-A[0].length);
+        double[][] A1 = new double[n][n];
+
         for (int i = 0; i < B.length; i++) {
             B[i][0] = matrix[i][matrix[i].length - 1];
         }
-
         
         
-        
-        if(A[0].length<elements.size()){
-            for(int i=0; i<matrix.length; i++){
-                matrix[i][matrix[i].length-1] = 1;
+        for(int i = 0; i < A.length; i++)
+        {
+            for(int j = 0; j < A[0].length; j++)
+            {
+                A1[i][j] = A[i][j];
             }
         }
 
-        int n = matrix.length;
-        double inverse[][] = invert(matrix);
-        double det = determinant(matrix, n);
+        if(A[0].length<A.length){
+            for(int i=0; i<n; i++){
+                for(int j = n-difference; j< n; j++)
+                {
+                    A1[i][j] = 1;
+                }
+            }
+        }
+        else if (A[0].length> A.length)
+        {
+            for(int i=0; i<n; i++){
+                for(int j = n-difference; j< n; j++)
+                {
+                    A1[j][i] = 1;
+                }
+            }
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=counter; j<n; j++){
+                matrix[i][j] = matrix[i][j] * -1;
+            }
+        }
+        
+        double det = determinant(A1, n);
+        double inverse[][] = invert(A1);
         double[][] prod = product(inverse, B, det);
 
-        System.out.println("Original Matrix \n ");
-        printMatrix(matrix);
-        System.out.println("\n");
+        double factor = 0;
+        boolean simplified = true;
+        for(int i = 0; i < prod.length; i++)
+        {
+            for(int j = i; j < prod.length; j++)
+            {
+                if(mod(prod[i][0],prod[j][0]))
+                {
+                    simplified = false;
+                    break;
+                }
+            }
+        }
 
-        System.out.println("The inverse is: ");
-        printMatrix(inverse);
-        System.out.println("\n");
+        if (simplified == false)
+        {
+            factor = findSmallest(prod);
+            simplify(prod, factor);
 
-        System.out.println("det is: " + det + "\n");
-        
-        System.out.println("prod A^(-1)*B*det is: " + n + "\n");
-        printMatrix(prod);
+        }
+        boolean subtract = false;
+
+        for(int j = 0; j < r1.length; j++)
+        {
+            if(j == r1.length-1)
+            {
+                int sum = 0;
+                int count = 0;
+                for(int m = 0; m < B[0].length; m++)
+                {
+                    if(B[m][0] == 0)
+                    {
+                        count++;
+                    }
+                }
+                for(int k = 0; k < n; k++)
+                {
+                    sum += Math.round(matrix[count][k]*Math.abs(prod[k][0]));
+
+                }
+
+                if(B[count][0] == 0)
+                {
+
+                    System.out.println(1 + " " + r2[j-2]);
+                }
+                else
+                {
+                    
+                    System.out.println(Math.abs(sum/(int)B[count][0]) + " " + r2[j-2]);
+                }
+            }
+            else if(r1[j].equals("=="))
+            {
+                System.out.print("--> ");
+                subtract = true;
+            }
+            else if (subtract == true)
+            {
+                int coeff = (int)Math.round(Math.abs(prod[j-1][0]));
+                System.out.print(coeff + " " + r1[j] + " ");
+            }
+            else
+            {
+                int coeff = (int)Math.round(Math.abs(prod[j][0]));
+                System.out.print(coeff + " " + r1[j] + " ");
+            }
+        }
 
     }
+
+    public static boolean mod(double a, double b) 
+    {
+
+  		int c = (int)(a)/(int)(b);
+  		if (c*b == a)
+  			return true;
+  		else
+  			return false;
+  	}
 
     public static void printMatrix(double[][] matrix)
     {
@@ -116,6 +208,47 @@ public class ChemLAB
             System.out.print("\n");
         }
     }
+
+    public static double findSmallest(double a[][])
+    {
+        double smallest = a[0][0];
+        for(int i = 0; i < a.length; i++)
+        {
+            if(Math.abs(a[i][0]) < Math.abs(smallest))
+                smallest = a[i][0];
+        }
+        return smallest;
+    }
+
+    public static double[][] simplify(double a[][], double smallest)
+    {
+        int largest = 0;
+        boolean all = true;
+        for(int i = 1; i <=  Math.abs(smallest); i++)
+        {
+            all = true;
+            for(int j = 0; j < a.length; j++)
+            {
+                if(!mod(a[j][0],i) )
+                {
+                    all = false;
+                }
+            }
+            if (Math.abs(i)>Math.abs(largest) && all == true)
+                largest = i;
+        }
+        if (debug == true)
+            System.out.println(largest);
+        if(largest!=0)
+        {
+            for(int k = 0; k < a.length; k++)
+            {
+                a[k][0] = a[k][0]/largest;
+            }
+        }
+        return a;
+    }
+
     public static double[][] product(double a[][], double b[][], double det)
     {
         int rowsInA = a.length;
@@ -182,17 +315,14 @@ public static double[][] invert(double a[][])
     for (int i=0; i<n; ++i) 
         b[i][i] = 1;
 
- // Transform the matrix into an upper triangle
     gaussian(a, index);
 
- // Update the matrix b[i][j] with the ratios stored
     for (int i=0; i<n-1; ++i)
         for (int j=i+1; j<n; ++j)
             for (int k=0; k<n; ++k)
                 b[index[j]][k]
             -= a[index[j]][i]*b[index[i]][k];
 
- // Perform backward substitutions
             for (int i=0; i<n; ++i) 
             {
                 x[n-1][i] = b[index[n-1]][i]/a[index[n-1]][n-1];
@@ -253,7 +383,7 @@ public static double[][] invert(double a[][])
                 int itmp = index[j];
                 index[j] = index[k];
                 index[k] = itmp;
-                for (int i=j+1; i<n; ++i) 	
+                for (int i=j+1; i<n; ++i)   
                 {
                     double pj = a[index[i]][j]/a[index[j]][j];
 
@@ -268,4 +398,6 @@ public static double[][] invert(double a[][])
         }
         public static void main(String[] args)
         {
-        Balance("MgO, Fe == Fe2O3, Mg");}}
+        	Balance("MgO, Fe == Fe2O3, Mg");
+        }
+    }
