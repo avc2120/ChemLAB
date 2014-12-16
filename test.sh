729e19f4b38c289bd9be713adc0a063de25e7c40
Martin Ong <mo2454@columbia.edu>
2014-12-15 20:35:03 -0500

    Updated test script

--- test.sh
+++ test.sh
@@ -3,19 +3,40 @@
 make
 
 TESTFILES="./test/*.chem"
+ran=0
+success=0
+fail=0
 
 Compare() {
-	diff -bq "$1" "$2" || echo "FAILED" 1>&2
+	diff -bq "$1" "$2" && { 
+		(( success++ ))
+		echo "PASS"
+	} || {
+		echo "FAILED" 1>&2
+		(( fail++ ))
+	}
 }
 
 for f in $TESTFILES
 do
+	(( ran++ ))
 	name=${f%.chem}			# remove .chem from the end
 	name=${name#./test/}	# remove ./test/ from the beginning
 	exp=${f%$name.chem}"exp/$name.out"		# insert exp/ into file path
-	echo
-	echo "##### Testing: $name"
+	echo "===================="
+	echo "Testing: $name"
 	./chemlab "$f" > "./test/$name.out" 2>&1
 	# echo "Comparing with $exp"
-	Compare "./test/$name.out" "$exp"
+	if [[ -e $exp ]]; then
+		Compare "./test/$name.out" "$exp"
+	else 
+		echo "FAILED: no output"
+		(( fail++ ))
+	fi
 done
+
+echo "===================="
+echo "SUMMARY"
+echo "Number of tests run: $ran"
+echo "Number Passed: $success"
+echo "Number Failed: $fail"
