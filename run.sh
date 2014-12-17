a5d00eb38853180b3f5aaf7d71957f7ed1170e47
Alice Chang <avc2120@columbia.edu>
2014-12-17 02:18:56 -0500

    changed file names so can rm files easily in Makefile

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TESTFILES="test/*.chem"
-CODETESTFILES="test/CodeTest/*.chem"
+TESTFILES="files_test/*.chem"
+CODETESTFILES="files_test/CodeTest/*.chem"
 ran=0
 success=0
 fail=0
@@ -20,19 +20,19 @@ Compare() {
 Test() {
 	(( ran++ ))
 	name=${f%.chem}			# remove .chem from the end
-	name=${name#test/}	# remove ./test/ from the beginning
+	name=${name#files_test/}	# remove ./files_test/ from the beginning
 	exp=${f%$name.chem}"exp/$name.out"		# insert exp/ into file path
 	echo "===================="
 	echo "Testing: $name"
-	./chemlab "$f" > "test/$name.out" 2>&1 && {
+	./chemlab "$f" > "files_test/$name.out" 2>&1 && {
 	# echo "Comparing with $exp"
 		# if [[ -e $exp ]]; then
-			Compare "test/$name.out" "$exp"
+			Compare "files_test/$name.out" "$exp"
 		# else 
 			# echo "FAILED: no expected file found"
 		# fi
 	} || {
-		cat "test/$name.out"
+		cat "files_test/$name.out"
 		echo "FAILED: did not compile"
 		(( fail++ ))
 	}
